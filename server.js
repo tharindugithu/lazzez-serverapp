196d2faf174eb5fc6424b74872ea27cd7ef90f83
D.L THARINDU MANDUSANKA <atharindu325@gmail.com>
2024-06-23 01:21:11 +0530

    test run1

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get("/api",(req,res)=>{
-     res.send("all good")
+     res.send("all good and done!")
 })
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
