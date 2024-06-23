d3e131b9890871afc2ae9d2ac75fbdbd423a9d18
D.L THARINDU MANDUSANKA <atharindu325@gmail.com>
2024-06-23 08:44:11 +0530

    test pipe

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get("/api",(req,res)=>{
-     res.send("all good and done!!")
+     res.send("all good and done & dusted!!")
 })
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
